dc41669d5ad1cd9630348db31ce3a6aaeee613c5
George Papadopoulos <georgepap2001@gmail.com>
2023-08-31 00:28:07 +0300

    Changed package permission fields to optional.

--- routes/package_permissions_routes.py
+++ routes/package_permissions_routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Form
-from typing import Annotated
+from typing import Annotated, Optional
 from models.package_permissions_model import PackagePermissions
 from schemas.package_permissions_schema import all_package_permissions_serializer
 from config.db import package_permissions_collection
@@ -7,16 +7,16 @@ from config.db import package_permissions_collection
 package_permission = APIRouter()
 
 
-@package_permission.post("/api/v1/package-permissions/add", status_code=201)
+@package_permission.post("/v1/package-permissions/add", status_code=201)
 async def add_package_permission(
-    device_token: Annotated[str, Form()],
-    package_name: Annotated[str, Form()],
-    app_name: Annotated[str, Form()],
-    permissions: Annotated[str, Form()],
-    certificate_subjects: Annotated[str, Form()],
-    certificate_issuers: Annotated[str, Form()],
-    certificate_serial_numbers: Annotated[str, Form()],
-    certificate_versions: Annotated[str, Form()]
+    device_token: Optional[str] = Form(None),
+    package_name: Optional[str] = Form(None),
+    app_name: Optional[str] = Form(None),
+    permissions: Optional[str] = Form(None),
+    certificate_subjects: Optional[str] = Form(None),
+    certificate_issuers: Optional[str] = Form(None),
+    certificate_serial_numbers: Optional[str] = Form(None),
+    certificate_versions: Optional[str] = Form(None)
 ):
     package_permission_model = PackagePermissions(
         device_token=device_token,
