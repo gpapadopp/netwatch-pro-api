df8907ac34b8a9d1e6488198f1e6bf0702531c8e
George Papadopoulos <georgepap2001@gmail.com>
2023-11-14 15:29:22 +0200

    Added authorization to package apk api endpoint.

--- routes/package_apks_routes.py
+++ routes/package_apks_routes.py
@@ -1,13 +1,17 @@
 from fastapi import APIRouter, UploadFile, File, Form
-from typing import Annotated
+from typing import Annotated, Optional
 from models.package_apks_model import PackageApks
 from schemas.package_apks_schema import all_package_apks_serializer, package_apks_serializer
 from config.db import package_apks_collection
 import uuid
 import hashlib
 import os
+from utils.permission_checker import PermissionChecker
+from enums.access_models_enum import AccessModelsEnum
 
 package_apk = APIRouter()
+
+permission_access_checker = PermissionChecker()
 
 
 @package_apk.post("/v1/package-apks/predict", status_code=201)
@@ -15,8 +19,22 @@ async def predict_package_apks(
         device_token: Annotated[str, Form()],
         package_name: Annotated[str, Form()],
         app_name: Annotated[str, Form()],
+        api_key: Optional[str] = Form(None),
+        secret_key: Optional[str] = Form(None),
         apk_file: UploadFile = File(...)
 ):
+    if api_key is None or secret_key is None:
+        return {
+            "status": "error",
+            "message": "unauthorized"
+        }
+
+    if not permission_access_checker.check_model_permission(AccessModelsEnum.PackagePermissionsModel, api_key, secret_key):
+        return {
+            "status": "error",
+            "message": "unauthorized"
+        }
+
     unique_filename = str(uuid.uuid4())
     apk_file_extension = apk_file.filename.split(".")[1]
     file_unique_name = unique_filename + "." + apk_file_extension
